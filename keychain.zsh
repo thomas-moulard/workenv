d2d0603aec1588c18a3cfd59e6dd30d2d663fef7
Thomas Moulard <thomas.moulard@gmail.com>
2011-11-15 11:28:36 +0100

    Make keychain optional.

--- keychain.zsh
+++ keychain.zsh
@@ -1,3 +1,6 @@
-keychain $KEYCHAIN_KEYS 
-. ~/.keychain/$HOST-sh
-. ~/.keychain/$HOST-sh-gpg
+# -*- shell-script -*-
+if test -x /usr/bin/keychain; then
+    keychain $KEYCHAIN_KEYS
+    test -f "$HOME/.keychain/$HOST-sh" && source "$HOME/.keychain/$HOST-sh"
+    test -f "$HOME/.keychain/$HOST-sh-gpg" && source "$HOME/.keychain/$HOST-sh-gpg"
+fi
